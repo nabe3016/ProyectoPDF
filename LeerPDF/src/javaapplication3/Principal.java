d98d590f60d2ec59d36fbf64559da1fbe54c262e
Benicio <bena3016@gmail.com>
2017-06-29 20:44:13 -0300

    Update Principal.java

--- LeerPDF/src/javaapplication3/Principal.java
+++ LeerPDF/src/javaapplication3/Principal.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-//VERSION ALFA 29-06-2017
+
 package javaapplication3;
 
 import java.awt.image.BufferedImage;
